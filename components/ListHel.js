23814bc596df5ce285623502b358f53852067424
miskang <miska.nghipandulwa@metropolia.fi>
2020-10-06 16:22:28 +0300

    public events now have images

--- components/ListHel.js
+++ components/ListHel.js
@@ -1,60 +1,62 @@
 import React, { useEffect, useState } from 'react';
-import {
-  FlatList,
-} from 'react-native';
+import { FlatList } from 'react-native';
 import ListItemHel from './ListItemHel';
 import PropTypes from 'prop-types';
 import { getComments, useLoadEvent } from '../hooks/APIhooks';
 
-
 const ListHel = ({ navigation }) => {
-  const [eventsHel, setEventsHel] = useState([])
+  const [eventsHel, setEventsHel] = useState([]);
 
-  const apiUrl = 'http://open-api.myhelsinki.fi/v1/events/?limit=5';
+  const apiUrl = 'http://open-api.myhelsinki.fi/v1/events/?limit=20';
   const loadEvent = async () => {
-
     try {
       const response = await fetch(apiUrl);
       const json = await response.json();
       const data = json.data;
-      let helEvents = []
+      let helEvents = [];
 
       for (let i = 0; i < data.length; i++) {
-        const name = data[i].name.fi
+        const name = data[i].name.fi;
         const description = data[i].description.intro;
         const dateTime = data[i].event_dates.starting_day;
         const address = data[i].location.address;
         const city = data[i].location.address.locality;
+        const images = null;
+        if (data[i].description.images[0] != null) {
+          images = data[i].description.images[0].url;
+        }
 
         const event = {
-          "name": name, "description": description, "datetime": dateTime, "address": address, "city": city
-        }
-        helEvents.push(event)
+          name: name,
+          description: description,
+          datetime: dateTime,
+          address: address,
+          city: city,
+          images: images,
+        };
+        helEvents.push(event);
       }
 
-      console.log(helEvents)
+      console.log(helEvents);
       setEventsHel(helEvents);
     } catch (e) {
       console.error(e);
     }
-
   };
-  useEffect(()=>{
-    loadEvent()
-  }, [])
-  console.log("here", eventsHel);
-
+  useEffect(() => {
+    loadEvent();
+  }, []);
+  console.log('here', eventsHel);
 
   return (
     <FlatList
       data={eventsHel}
       keyExtractor={(item, index) => index.toString()}
-      renderItem={({ item }) =>
+      renderItem={({ item }) => (
         <ListItemHel navigation={navigation} singleEvent={item} />
-      }>
-    </FlatList>
-  )
+      )}
+    ></FlatList>
+  );
 };
-
 
 export default ListHel;
--- components/ListItemHel.js
+++ components/ListItemHel.js
@@ -1,25 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { StyleSheet, Text,  Image } from 'react-native';
-import {ListItem as CoolListItem, Thumbnail, Left, Body, Right, Button, Icon } from 'native-base';
+import { StyleSheet, Text, Image } from 'react-native';
+import {
+  ListItem as CoolListItem,
+  Thumbnail,
+  Left,
+  Body,
+  Right,
+  Button,
+  Icon,
+} from 'native-base';
 
-const ListItem = ({navigation, singleEvent}) => {
-  
-
+const ListItem = ({ navigation, singleEvent }) => {
   return (
-
-        <CoolListItem thumbnail>
-           <Body>
-            <Text>{singleEvent.name}</Text>
-            <Text>{singleEvent.city}</Text>
-            <Text>{singleEvent.datetime}</Text>
-
-          </Body>
-          
-          
-        </CoolListItem>
- )};
+    <CoolListItem thumbnail>
+      <Left>
+        {singleEvent.images != null && (
+          <>
+            <Thumbnail square source={{ uri: singleEvent.images }} />
+          </>
+        )}
+      </Left>
+      <Body>
+        <Text>{singleEvent.name}</Text>
+        <Text>{singleEvent.city}</Text>
+        <Text>{singleEvent.datetime}</Text>
+      </Body>
+    </CoolListItem>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
